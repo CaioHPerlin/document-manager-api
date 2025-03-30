099cc836dbb28198507de172e68b35a6131876b1
CaioHPerlin <caiohperlin@gmail.com>
2025-03-30 12:47:20 -0400

    chore: remove placeholder error

--- src/api/v1/services/HealthService.ts
+++ src/api/v1/services/HealthService.ts
@@ -1,10 +1,7 @@
-import { ServiceUnavailableError } from "@/errors";
 import { HealthStatus } from "../interfaces";
 
 class HealthService {
 	public static get healthStatus(): HealthStatus {
-		throw new ServiceUnavailableError();
-
 		return {
 			status: "UP",
 			version: "1.0.0",
